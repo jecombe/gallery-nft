b91f31c4a7024f7e3073b1f7c08e274fe3b85e4f
Jeremy Combe <jeremycombe@MacBook-Air-de-Jeremy.local>
2023-04-25 15:43:10 +0200

    updt config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    largePageDataBytes: 800 * 1000,
-  },
 }
 
 module.exports = nextConfig
